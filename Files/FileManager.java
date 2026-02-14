967608498f15d8da30f472d24af7864ff741d016
Faniry <faniryanthony94@gmail.com>
2026-02-14 22:04:41 +0300

    "voici le receive"

--- Files/FileManager.java
+++ Files/FileManager.java
@@ -1,6 +1,9 @@
 package Files;
 
+import java.io.DataInputStream;
 import java.io.File;
+import java.io.FileOutputStream;
+import java.net.Socket;
 import java.util.*;
 
 public class FileManager {
@@ -17,5 +20,19 @@ public class FileManager {
         File dossier = new File(FOLDER);
         String[] files = dossier.list();
         return files != null ? new ArrayList<>(Arrays.asList(files)) : new ArrayList<>();
+    }
+
+    public static void receiveFile(Socket socket) throws Exception {
+        DataInputStream di = new DataInputStream(socket.getInputStream());
+        String filename = di.readUTF();
+        try (FileOutputStream fos = new FileOutputStream("recu_"+filename)) {
+            byte[] buffer = new byte[4096];
+            int bytesread;
+
+            while ((bytesread = di.read(buffer)) != -1) {
+                fos.write(buffer, 0, bytesread);
+            }
+            System.out.println("Fichier recu avec succes");
+        } 
     }
 }
